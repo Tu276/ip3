64788aa87881c1fd549a77072f7184e943f0e929
Tu276 <tu276nathan@gmail.com>
2020-08-05 17:28:06 +0300

    add js cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,24 @@
-$(document).ready(function(){
-  $("#designIcon").click(function(){
-      $(".design-a").hide().toggle();
-      $("#designIcon").slideUp();    
-          });
-  $(".design-a").click(function(){
-      $("#designIcon").slideDown();
-      $(".design-a").hide();
-  })
-  
-  
-  $("#devIcon").click(function(){
-      $(".design-b").hide().toggle();
-      $("#devIcon").slideUp();    
-          });
-      $(".design-b").click(function(){
-          $("#devIcon").slideDown();
-          $(".design-b").hide();
-      });
-  
- 
-  $("#productIcon").click(function(){
-      $(".design-c").hide().toggle();
-      $("#productIcon").slideUp();    
-          });
-      $(".design-c").click(function(){
-          $("#productIcon").slideDown();
-          $(".design-c").hide();
-      });
-});  
-
-
-$(document).ready(function(){
-  $('.card0').mouseover(function (){
-      $('.project').show();
-  }); 
-  $('.card0').mouseout(function(){
-      $('.project').hide();
-  });
-
-  $('.card1').mouseover(function (){
-      $('.project2').show();
-  }); 
-  $('.card1').mouseout(function(){
-      $('.project2').hide();
-  });
-
-  $('.card2').mouseover(function (){
-      $('.project3').show();
-  }); 
-  $('.card2').mouseout(function(){
-      $('.project3').hide();
-  });
-
-  $('.card3').mouseover(function (){
-      $('.project4').show();
-  }); 
-  $('.card3').mouseout(function(){
-      $('.project4').hide();
-  });
-
-  $('.card4').mouseover(function (){
-      $('.project5').show();
-  }); 
-  $('.card4').mouseout(function(){
-      $('.project5').hide();
-  });
-
-  $('.card5').mouseover(function (){
-      $('.project6').show();
-  }); 
-  $('.card5').mouseout(function(){
-      $('.project6').hide();
-  });
-
-  $('.card6').mouseover(function (){
-      $('.project7').show();
-  }); 
-  $('.card6').mouseout(function(){
-      $('.project7').hide();
+{
+    
+}
+$('.main').click(function () {
+    $(this).children('.hide').slideToggle(1000);
+    
+    $(this).children('.show').toggle(2000);
   });
   
-  $('.card7').mouseover(function (){
-      $('.project8').show();
-  }); 
-  $('.card7').mouseout(function(){
-      $('.project8').hide();
+
+
+
+$(".card").hover(function () {
+    $(this).children(".card-1").fadeToggle(1000, "linear");
   });
-});
 
-function contact(form){
-  var name = document.forms["contacts"]["name"].value;
-  var email = document.forms["contacts"]["email"].value;
-  var message = document.forms["contacts"]["message"].value;
-  alert("Hello! " + name + "," + " We have received your message. Thank you for reaching out to us. ");
-  }
+  
 
+
+$("button").click(function (getDetails) {
+    var client = document.getElementById('nameDetail').value;
+    alert(' Dear ' + client + ' Thank you for reaching out. We will get back to you within 24 hours max.' + ' Delani Studio Team ');
+    getDetails.preventDefault();
+  });
